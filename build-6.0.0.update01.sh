f2494b359706cecda0337d952ffcb3c24fa8791b
Ivan Porto Carrero <ivan@flanders.co.nz>
2015-12-04 09:49:24 -0800

    add build script and some ignores

---
+++ build-6.0.0.update01.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+packer build \
+       -var 'iso_file=iso/VMware-VMvisor-Installer-6.0.0.update01-3029758.x86_64.iso' \
+       -var 'iso_sha1sum=e2b73cf0bb3859b49643750bf21cab802a6a6918' \
+       packer-esxi.json

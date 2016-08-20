582e35e5728370e5565083856c0f09e1da885733
Rickard von Essen <rickard.von.essen@gmail.com>
2016-08-20 17:24:33 +0200

    Cleanup the template
    - Allow to select checksum _type_. Defaults to `none`.
    - Allow to specify _disk size_

--- build-6.0.0.update01.sh
+++ build-6.0.0.update01.sh
@@ -2,5 +2,6 @@
 
 packer build \
        -var 'iso_file=iso/VMware-VMvisor-Installer-6.0.0.update01-3029758.x86_64.iso' \
-       -var 'iso_sha1sum=e2b73cf0bb3859b49643750bf21cab802a6a6918' \
+       -var 'iso_checksum_type=sha1' \
+       -var 'iso_checksum=e2b73cf0bb3859b49643750bf21cab802a6a6918' \
        packer-esxi.json
